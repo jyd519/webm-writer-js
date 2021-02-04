f08a4061f14cba4dd4ebf4d153a4d28f93250192
jyd519 <jiyongdong@ata.net.cn>
2021-02-04 17:36:06 +0800

    fix: for electron v11

--- test/electron/main.js
+++ test/electron/main.js
@@ -1,43 +1,48 @@
-const electron = require('electron');
+const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path');
-const url = require('url');
+const path = require("path");
+const url = require("url");
 
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 800,
-    title: "WebM writer test"
+    title: "WebM writer test",
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
   });
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'test.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "test.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
 
   mainWindow.webContents.openDevTools();
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
-
